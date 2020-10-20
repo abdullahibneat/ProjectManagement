4e742e32c26705aed19662f49b142904e9a5d37f
Abdullah Ibne Atiq <hello@abdu.io>
2020-10-20 15:38:28 +0100

    Create Task class

---
+++ src/Kotlin/Task.kt
@@ -0,0 +1,16 @@
+class Task(var name: String, var duration: Int, val previousTasks: ArrayList<Task> = arrayListOf()) {
+    val nextTasks = arrayListOf<Task>()
+
+    init {
+        previousTasks.forEach { t -> t.nextTasks.add(this) }
+    }
+
+    override fun toString() = """
+        
+        Name: $name
+        Duration: $duration
+        Previous tasks: [${previousTasks.map { t -> t.name }.joinToString()}]
+        Next tasks: [${nextTasks.map { t -> t.name }.joinToString()}]
+        
+    """.trimIndent()
+}
