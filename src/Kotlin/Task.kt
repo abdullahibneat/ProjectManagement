197099a59b25b07e582df654afd930bc53cf062e
Abdullah Ibne Atiq <hello@abdu.io>
2020-10-22 11:24:12 +0100

    Return only task name in toString()

--- src/Kotlin/Task.kt
+++ src/Kotlin/Task.kt
@@ -5,12 +5,14 @@ class Task(var name: String, var duration: Int, val previousTasks: ArrayList<Tas
         previousTasks.forEach { t -> t.nextTasks.add(this) }
     }
 
-    override fun toString() = """
-        
-        Name: $name
-        Duration: $duration
-        Previous tasks: [${previousTasks.map { t -> t.name }.joinToString()}]
-        Next tasks: [${nextTasks.map { t -> t.name }.joinToString()}]
-        
-    """.trimIndent()
+    override fun toString() = name
+
+//    override fun toString() = """
+//
+//        Name: $name
+//        Duration: $duration
+//        Previous tasks: [${previousTasks.map { t -> t.name }.joinToString()}]
+//        Next tasks: [${nextTasks.map { t -> t.name }.joinToString()}]
+//
+//    """.trimIndent()
 }
