3000d125118d17ed23efb82c2601b494254ba299
Abdullah Ibne Atiq <hello@abdu.io>
2020-10-23 11:23:20 +0100

    Refactor fun criticalPath(), add comments.
    The code for forward and backward pass was almost identical. The 2 have now been merged. Added comments explaining how both passes work.

--- src/Kotlin/CriticalPath.kt
+++ src/Kotlin/CriticalPath.kt
@@ -13,46 +13,54 @@ data class Calculations(var earlyStart: Int, var earlyFinish: Int, var lateStart
 
 fun criticalPath(tasks: Set<Task>) {
     val computed = HashMap<Task, Calculations>()
-    var toCompute = tasks.toMutableList()
+    val toCompute = tasks.toMutableList()
 
-    // Forward pass
+    var forwardPass = true // Begin with forward pass
+
     while(toCompute.isNotEmpty()) {
-        val next = toCompute[0]
+        val current = toCompute.removeAt(0)
 
-        // Try to find all previous tasks that have been computed
+        val expectedNumberOfDependencies = if(forwardPass) current.previousTasks.size else current.nextTasks.size
         val dependencies = arrayListOf<Calculations>()
-        for(t in next.previousTasks) computed[t]?.let { dependencies.add(it) }
 
-        // If all previous tasks have been already calculated, this task can now be processed
-        if(dependencies.size == next.previousTasks.size) {
-            val prevEarlyFinish = if(dependencies.size > 0 ) dependencies.maxOf { t -> t.earlyFinish } else 0
-            computed[next] = Calculations(prevEarlyFinish + 1, prevEarlyFinish + next.duration)
-            toCompute.removeAt(0)
-        } else {
-            // This task cannot be computed now, move on to next task by moving this task to the end of the array.
-            toCompute.removeAt(0)
-            toCompute.add(next)
+        // Try to find all previous/successor tasks that have been already computed
+        if(forwardPass) for(t in current.previousTasks) computed[t]?.let { dependencies.add(it) } ?: break
+        else for(t in current.nextTasks) computed[t]?.let { if(it.lateStart !== null && it.lateFinish !== null) dependencies.add(it) } ?: break
+
+        if(dependencies.size != expectedNumberOfDependencies) {
+            toCompute.add(current) // This task cannot be computed now, try later.
+            continue
         }
-    }
 
-    // Backward pass
-    toCompute = tasks.toMutableList()
-    while (toCompute.isNotEmpty()) {
-        val next = toCompute[0]
+        if(forwardPass) {
+            // Forward pass requires previous tasks' early finish times.
+            // NOTE: Since the starting task (A) has no dependencies, set the value to 0.
+            // For the current task:
+            //  - the early start time is calculated by adding 1 to the whichever dependant task
+            //    will finish last.
+            //  - the early finish time is computed with the equation: this.earlyStart + this.duration - 1
+            //                              This can be simplified: = (prevEarlyFinish + 1) + this.duration - 1 (cancel out the 1s: 1-1=0)
+            //                                                      = prevEarlyFinish + this.duration
 
-        // Find all the successor tasks that have been computed (by checking if lateStart and lateFinish are defined)
-        val dependencies = arrayListOf<Calculations>()
-        for(t in next.nextTasks) computed[t]?.let { if(it.lateStart !== null && it.lateFinish !== null) dependencies.add(it) }
+            val prevEarlyFinish = if (dependencies.size > 0) dependencies.maxOf { t -> t.earlyFinish } else 0
+            computed[current] = Calculations(prevEarlyFinish + 1, prevEarlyFinish + current.duration)
 
-        // If all successor tasks have been computed, this task can also be computed
-        if(dependencies.size == next.nextTasks.size) {
-            val nextLateFinish = if(dependencies.size > 0 ) dependencies.minOf { t -> t.lateStart!! } else computed[next]!!.earlyFinish + 1
-            computed[next]!!.lateFinish = nextLateFinish - 1
-            computed[next]!!.lateStart = nextLateFinish - next.duration
-            toCompute.removeAt(0)
+            if(current.nextTasks.isEmpty() && computed.contains(current)) forwardPass = false   // If this is the END task (no successor tasks)
+                                                                                                // AND it's early start and finish values have been computed, forward pass is completed.
+
+            toCompute.add(current) // Add task to end of list to compute the backward pass.
         } else {
-            toCompute.removeAt(0)
-            toCompute.add(next)
+            // Backward pass requires the successor tasks' late start times.
+            // NOTE: Since the very last task has no successor tasks, set the value to: earlyFinish + 1
+            // For the current task:
+            //  - the late finish time is calculated by subtracting 1 from whichever successor task
+            //    finishes first.
+            //  - the late start time is computed with the equation: this.lateFinish - this.duration + 1
+            //    Like in the forward pass, this is simplified to:   nextLateStart - current.duration
+
+            val nextLateStart = if(dependencies.size > 0 ) dependencies.minOf { t -> t.lateStart!! } else computed[current]!!.earlyFinish + 1
+            computed[current]!!.lateFinish = nextLateStart - 1
+            computed[current]!!.lateStart = nextLateStart - current.duration
         }
     }
 
