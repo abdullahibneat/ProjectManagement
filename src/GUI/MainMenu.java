422b52a4349bab150b4fb202450b677d7569402f
Ed Bencito <RusselBencito@gmail.com>
2020-11-04 15:35:28 +0000

    Reworked MainMenu.form

--- src/GUI/MainMenu.java
+++ src/GUI/MainMenu.java
@@ -2,21 +2,25 @@ package GUI;
 
 import javax.swing.*;
 
-public class MainMenu extends JFrame {
-
-    private JPanel Base;
-    private JPanel Panel_Greeting;
-    private JPanel Panel_Project_Title;
-    private JPanel Panel_Projects;
-    private JPanel Panel_Teams_Title;
+public class MainMenu {
+    private JPanel BasePanel;
+    private JButton ProjectsAddButton;
+    private JButton TeamsAddButton;
+    private JPanel GreetingPanel;
+    private JPanel ProjectsPanel;
+    private JPanel TeamsPanel;
+    private JPanel TeamsSubPanel;
+    private JPanel ProjectsSubPanel;
+    private JLabel ProjectsLabel;
+    private JLabel TeamsLabel;
+    private JScrollPane ProjectsScrollPane;
+    private JScrollPane TeamsScrollPane;
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Main_Menu");
-        frame.setContentPane(new MainMenu().Base);
+        JFrame frame = new JFrame("MainMenu");
+        frame.setContentPane(new MainMenu().BasePanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
     }
-
-    private JPanel Panel_Teams;
 }
