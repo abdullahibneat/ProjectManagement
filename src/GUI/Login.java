5c666cda2af45d7b27cfb3281dea83d5e1449bd0
Ed Bencito <RusselBencito@gmail.com>
2020-11-04 16:46:21 +0000

    More Login work

--- src/GUI/Login.java
+++ src/GUI/Login.java
@@ -4,12 +4,16 @@ import javax.swing.*;
 
 public class Login {
     private JPanel panel1;
-    private JTextField textField1;
-    private JPasswordField passwordField1;
-    private JButton button1;
+    private JTextField UsernameTextField;
+    private JPasswordField PasswordField;
+    private JButton LoginButton;
+    private JLabel UsernameLabel;
+    private JLabel PasswordLabel;
+    private JLabel ApplicationTitleLabel;
+    private JPanel CedentialsPanel;
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Login");
+        JFrame frame = new JFrame("LoginButton");
         frame.setContentPane(new Login().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
