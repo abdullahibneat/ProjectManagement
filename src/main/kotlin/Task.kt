9b943cc0147eabe7cb2152aefaf10f6ad797ce33
Abdullah Ibne Atiq <hello@abdu.io>
2020-11-05 10:32:23 +0000

    Uncomment lines

--- src/main/kotlin/Task.kt
+++ src/main/kotlin/Task.kt
@@ -1,18 +1,18 @@
-//class Task(var name: String, var duration: Int, val previousTasks: ArrayList<Task> = arrayListOf()) {
-//    val nextTasks = arrayListOf<Task>()
+class Task(var name: String, var duration: Int, val previousTasks: ArrayList<Task> = arrayListOf()) {
+    val nextTasks = arrayListOf<Task>()
+
+    init {
+        previousTasks.forEach { t -> t.nextTasks.add(this) }
+    }
+
+    override fun toString() = name
+
+//    override fun toString() = """
 //
-//    init {
-//        previousTasks.forEach { t -> t.nextTasks.add(this) }
-//    }
+//        Name: $name
+//        Duration: $duration
+//        Previous tasks: [${previousTasks.map { t -> t.name }.joinToString()}]
+//        Next tasks: [${nextTasks.map { t -> t.name }.joinToString()}]
 //
-//    override fun toString() = name
-//
-////    override fun toString() = """
-////
-////        Name: $name
-////        Duration: $duration
-////        Previous tasks: [${previousTasks.map { t -> t.name }.joinToString()}]
-////        Next tasks: [${nextTasks.map { t -> t.name }.joinToString()}]
-////
-////    """.trimIndent()
-//}
+//    """.trimIndent()
+}
