d893daef6685f2a90b1a2984e3c7d3634f6047d8
Abdullah Ibne Atiq <hello@abdu.io>
2020-11-07 15:04:50 +0000

    Add tests to check that tasks can be edited

--- src/test/ProjectTest.kt
+++ src/test/ProjectTest.kt
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertTrue
 
 class ProjectTest {
     @Test
@@ -104,5 +105,72 @@ class ProjectTest {
         project.addTask("d", 3, "c")
         project.addTask("e", 2, "b", "d")
         assertThrows<Exception> { project.deleteTask("c") }
+    }
+
+    @Test
+    fun `A task's name and duration can be updated`() {
+        val project = Project("Sample Project A")
+        project.addTask("a", 6)
+        project.addTask("b", 11, "a")
+        project.addTask("c", 7, "a")
+        project.addTask("d", 3, "c")
+        project.addTask("e", 2, "b", "d")
+
+        // Check that task "a" exists and task "root" does NOT exist
+        assertTrue(project.tasks.find { t -> t.name === "a" } !== null)
+        assertTrue(project.tasks.find { t -> t.name === "root" } == null)
+
+        assertDoesNotThrow { project.editTask("a", "root") }
+
+        // Check that task "a" does NOT exists anymore and task "root" exists
+        assertTrue(project.tasks.find { t -> t.name === "a" } == null)
+        assertTrue(project.tasks.find { t -> t.name === "root" } !== null)
+
+        // Check that root has duration of 6
+        assertTrue(project.tasks.find { t -> t.name == "root" }?.duration == 6)
+
+        assertDoesNotThrow { project.editTask("root", newDuration = 7) }
+
+        // Check that root duration has been updated to 7
+        assertTrue(project.tasks.find { t -> t.name == "root" }?.duration == 7)
+    }
+
+    @Test
+    fun `Task name cannot be changed to existing name`() {
+        val project = Project("Sample Project")
+        project.addTask("a", 6)
+        project.addTask("b", 11, "a")
+        project.addTask("c", 7, "a")
+        project.addTask("d", 3, "c")
+        project.addTask("e", 2, "b", "d")
+
+        assertThrows<Exception> { project.editTask("a", "b") }
+    }
+
+    @Test
+    fun `Task name cannot be changed to be empty`() {
+        val project = Project("Sample Project")
+        project.addTask("a", 6)
+        project.addTask("b", 11, "a")
+        project.addTask("c", 7, "a")
+        project.addTask("d", 3, "c")
+        project.addTask("e", 2, "b", "d")
+
+        assertThrows<Exception> { project.editTask("a", "") }
+        assertThrows<Exception> { project.editTask("a", " ") }
+    }
+
+    @Test
+    fun `Task duration cannot be changed to be less than 1`() {
+        val project = Project("Sample Project")
+        project.addTask("a", 6)
+        project.addTask("b", 11, "a")
+        project.addTask("c", 7, "a")
+        project.addTask("d", 3, "c")
+        project.addTask("e", 2, "b", "d")
+
+        assertDoesNotThrow { project.editTask("a", newDuration = 1) }
+        assertThrows<Exception> { project.editTask("a", newDuration = 0) }
+        assertThrows<Exception> { project.editTask("a", newDuration = -1) }
     }
 }
