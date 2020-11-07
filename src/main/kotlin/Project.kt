d601b12c18e855e3570b3f208a11fdc910ddffea
Abdullah Ibne Atiq <hello@abdu.io>
2020-11-07 14:49:36 +0000

    Add function to edit a task

--- src/main/kotlin/Project.kt
+++ src/main/kotlin/Project.kt
@@ -28,6 +28,32 @@ class Project(projectName: String) {
         tasks.add(newTask)
     }
 
+    fun editTask(name: String, newName: String? = null, newDuration: Int? = null) {
+        val trimmedName = name.trim()
+
+        if(trimmedName.isEmpty())
+            throw Exception("Task name cannot be empty")
+
+        val task = tasks.find { t -> t.name === trimmedName }
+                ?: throw Exception("Task does not exist")
+
+        // If changing name, check if name is already in use
+        if(newName !== null && tasks.find { t -> t.name == newName.trim() } !== null)
+            throw Exception("Task name must be unique")
+
+        val originalName = task.name
+        val originalDuration = task.duration
+
+        // Try setting new values, if exception occurs revert changes.
+        try {
+            if(newName !== null) task.name = newName
+            if(newDuration !== null) task.duration = newDuration
+        } catch (e: Exception) {
+            task.name = originalName
+            task.duration = originalDuration
+        }
+    }
+
     fun deleteTask(name: String) {
         val trimmedName = name.trim()
 
