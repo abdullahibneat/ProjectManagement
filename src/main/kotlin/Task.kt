0f1888ea15e497b3740a874c897f3ca8591923dc
Abdullah Ibne Atiq <hello@abdu.io>
2020-11-14 15:35:30 +0000

    Use "var lag" in constructor

--- src/main/kotlin/Task.kt
+++ src/main/kotlin/Task.kt
@@ -1,4 +1,4 @@
-class Task(taskName: String, taskDuration: Int, val previousTasks: MutableSet<Task> = mutableSetOf(), Lag: Int = 0) {
+class Task(taskName: String, taskDuration: Int, val previousTasks: MutableSet<Task> = mutableSetOf(), var lag: Int = 0) {
     // Make sure name is never empty
     var name = ""
         set(value) {
@@ -11,19 +11,13 @@ class Task(taskName: String, taskDuration: Int, val previousTasks: MutableSet<Ta
             field = value
         }
 
-    var lag = 0
-
-
     val nextTasks = mutableSetOf<Task>()
 
     init {
         name = taskName
         duration = taskDuration
 
-
         previousTasks.forEach { t -> t.nextTasks.add(this) }
-
-        lag = Lag
     }
 
     override fun toString() = name
