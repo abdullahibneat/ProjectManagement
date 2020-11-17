8913344dd32116ff6033308463242800e8fff15a
Haren <hupadh4@gmail.com>
2020-11-17 16:07:41 +0000

    Creation of toJSON functions for TaskJSON and ProjectJSON classes

--- src/main/kotlin/Persistence.kt
+++ src/main/kotlin/Persistence.kt
@@ -2,18 +2,39 @@ import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 
 class TaskJSON(
-        val name: String,
-        val PreviousTasks: List<String>,
-        val NextTasks: List<String>,
-        val duration: Int,
-        val lag: Int
+        var name: String,
+        var PreviousTasks: List<String>,
+        var NextTasks: List<String>,
+        var duration: Int,
+        var lag: Int
 ){
+    fun toJSON(tasks: Task){
 
+        val NTList:List<String> = tasks.nextTasks.map { it.toString() }
+
+        name = tasks.name
+        NextTasks = NTList
+        duration = tasks.duration
+        lag = tasks.lag
+    }
 }
 
 class ProjectJSON(
         val name: String,
-        val tasks: TaskJSON
+        val task: List<TaskJSON>
 ){
+    fun toJSON(Project: Project){
 
+
+
+    }
+
+}
+
+object Persistence{
+
+    open fun addJSON(Project: Project) {
+
+
+    }
 }
