d135c2f91146fbd36ddb0347de373e147540b145
Haren <hupadh4@gmail.com>
2020-11-17 14:09:11 +0000

    Created TaskJSON and ProjectJSON classes

--- src/main/kotlin/Persistence.kt
+++ src/main/kotlin/Persistence.kt
@@ -1,2 +1,19 @@
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+
+class TaskJSON(
+        val name: String,
+        val PreviousTasks: List<String>,
+        val NextTasks: List<String>,
+        val duration: Int,
+        val lag: Int
+){
+
+}
+
+class ProjectJSON(
+        val name: String,
+        val tasks: TaskJSON
+){
+
+}
