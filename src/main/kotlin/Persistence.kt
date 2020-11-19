a1087405eb94b7b95ee3328b4cec03b102a982e1
Haren <hupadh4@gmail.com>
2020-11-19 17:09:15 +0000

    Implementation of lambda Method

--- src/main/kotlin/Persistence.kt
+++ src/main/kotlin/Persistence.kt
@@ -1,35 +1,51 @@
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 
-class TaskJSON(
+class taskJSON(
         var name: String,
         var PreviousTasks: List<String>,
-        var NextTasks: List<String>,
+        var nextTasks: List<String>,
         var duration: Int,
         var lag: Int
-){
-    fun toJSON(tasks: Task){
+)
 
-        val NTList:List<String> = tasks.nextTasks.map { it.toString() }
 
-        name = tasks.name
-        NextTasks = NTList
-        duration = tasks.duration
-        lag = tasks.lag
-    }
-}
-
-class ProjectJSON(
+class projectJSON(
         val name: String,
-        val task: List<TaskJSON>
-){
-    fun toJSON(Project: Project){
+        var taskList: List<taskJSON>
+)
 
 
 
-    }
+//fun Task.toJSON(tasks: Task): taskJSON {
+//    var previousTasksJson: List<String>
+//    var nextTasksJson: List<String>
+//
+//    val NTList: List<String> = tasks.nextTasks.map { it.toString() }
+//    val PTList: List<String> = tasks.previousTasks.map { it.toString() }
+//
+//    name = tasks.name
+//    previousTasksJson = PTList
+//    nextTasksJson = NTList
+//    duration = tasks.duration
+//    lag = tasks.lag
+//
+//    return taskJSON(name, previousTasksJson, nextTasksJson, duration, lag)
+//}
 
-}
+//fun Project.toJSON(Project: Project){
+//
+//    var taskList: MutableList<taskJSON> = mutableListOf()
+//
+//    for (tasks in Project.tasks){
+//
+//        taskList.add(Task().toJSON(tasks))
+//    }
+//
+//}
+
+fun Task.toJSON() = taskJSON(name, previousTasks.map { t -> t.name }, nextTasks.map { t -> t.name }, duration, lag)
+fun Project.toJSON() = projectJSON(name, tasks.map { t -> t.toJSON() } )
 
 object Persistence{
 
