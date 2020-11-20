18713310b5aa37fbd554af842f3a7b35e3138731
Abdullah Ibne Atiq <hello@abdu.io>
2020-11-20 11:21:42 +0000

    Add members and teams to output

--- src/main/kotlin/Persistence.kt
+++ src/main/kotlin/Persistence.kt
@@ -2,11 +2,17 @@ import java.io.File
 import com.google.gson.GsonBuilder
 
 fun main() {
-    val project = Project("Sample Project")
+    val team = Team("0+0=0")
+    team.addMember(Member("Haren"))
+
+    val project = Project("Sample Project", team)
     project.addTask("a", 6)
     project.addTask("b", 11, "a")
     project.addTask("c", 7, "a")
     project.addTask("d", 3, "c")
+
+    team.addMember(Member("Abdullah"))
+
     project.addTask("e", 2, "b", "d")
 
     val project2 = Project("Sample Project 2")
@@ -15,8 +21,6 @@ fun main() {
     project2.addTask("c 2", 7, "a 2")
     project2.addTask("d 2", 3, "c 2")
     project2.addTask("e 2", 2, "b 2", "d 2")
-
-    project.team = Team("Sample team")
 }
 
 data class TaskJSON(
@@ -73,10 +77,11 @@ object Persistence{
     }
 
     fun save() {
-
         val jsonFormatted = GsonBuilder().setPrettyPrinting().create()
 
-        val jsonOutput: String = jsonFormatted.toJson(projects.toList())
+        val toOutput = mapOf("projects" to projects.toList(), "members" to members.toList(), "teams" to teams.toList())
+
+        val jsonOutput: String = jsonFormatted.toJson(toOutput)
 
         File("data.json").writeText(jsonOutput)
 
