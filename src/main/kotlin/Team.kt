c748fdcaa4d48004ce60a90d1fbbe9b171da04c0
Abdullah Ibne Atiq <hello@abdu.io>
2020-11-20 11:20:48 +0000

    Automatically save teams to data.json when created/edited

--- src/main/kotlin/Team.kt
+++ src/main/kotlin/Team.kt
@@ -4,16 +4,21 @@ class Team(teamName: String) {
         set(value) {
             if (value.trim().isEmpty()) throw Exception("Name cannot be empty.")
             field = value.trim()
+            Persistence.updateTeam(this)
         }
 
     val members = mutableListOf<Member>()
 
     init {
         name = teamName
+        Persistence.addTeam(this)
     }
 
     fun addMember(member: Member) {
         if(members.contains(member)) throw Exception("Member is already part of $name team")
-        else members.add(member)
+        else {
+            members.add(member)
+            Persistence.updateTeam(this)
+        }
     }
 }
