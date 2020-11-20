bb436a6c0085fa0e8f542721b2ec72c56cb786a8
Abdullah Ibne Atiq <hello@abdu.io>
2020-11-20 10:18:00 +0000

    Convert TaskJSON and ProjectJSON to immutable data classes

--- src/main/kotlin/Persistence.kt
+++ src/main/kotlin/Persistence.kt
@@ -1,21 +1,18 @@
 import java.io.File
 import com.google.gson.GsonBuilder
 
-class TaskJSON(
-        var name: String,
-        var PreviousTasks: List<String>,
-        var nextTasks: List<String>,
-        var duration: Int,
-        var lag: Int
-)
-
-
-class ProjectJSON(
+data class TaskJSON(
         val name: String,
-        var taskList: List<TaskJSON>
+        val PreviousTasks: List<String>,
+        val nextTasks: List<String>,
+        val duration: Int,
+        val lag: Int
 )
 
-
+data class ProjectJSON(
+        val name: String,
+        val taskList: List<TaskJSON>
+)
 
 fun Task.toJSON() = TaskJSON(name, previousTasks.map { t -> t.name }, nextTasks.map { t -> t.name }, duration, lag)
 fun Project.toJSON() = ProjectJSON(name, tasks.map { t -> t.toJSON() } )
